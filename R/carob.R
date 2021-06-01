f6a43c48a6a8e602b34b183e0fbaf963c6553048
rhijmans <r.hijmans@gmail.com>
2021-06-01 12:38:10 -0700

    man

--- R/carob.R
+++ R/carob.R
@@ -147,7 +147,74 @@ compile_carob <- function(path) {
 	utils::write.csv(y, file.path(path, "data", "carob.csv"), row.names=FALSE)
 }
 
+
+get_packages <- function(path) {
+	libfun1 <- function(x) {
+		d <- readLines(x, warn=FALSE)
+		i <- grep('^library\\(', d)
+		pkgs <- d[unique(i)]
+		pkgs <- pkgs[!grepl("#", pkgs)]
+		pkgs <- pkgs[nchar(pkgs) < 100]
+		pkgs <- gsub("library\\(", "", pkgs)
+		pkgs <- trimws(gsub(")", "", pkgs))
+		pkgs <- sort(unique(pkgs))
+		pkgs <- gsub('\"', "", pkgs)
+	}
+	
+	libfun2 <- function(x) {
+		d <- readLines(x, warn=FALSE)
+		i <- grep('::', d)
+		d <- d[unique(i)]
+		d <- d[!grepl("#", d)]
+
+		d <- strsplit(d, "<-")
+		d <- sapply(d, function(e) ifelse(length(e)>1, e[2], e[1]))
+		d <- d[!is.na(d)]
+
+		d <- strsplit(d, "=")
+		d <- sapply(d, function(e) ifelse(length(e)>1, e[2], e[1]))
+		d <- d[!is.na(d)]
+
+		d <- strsplit(d, "::")
+		d <- sapply(d, function(e) e[1])
+		d <- unique(trimws(d[!is.na(d)]))
+		d <- d[!(grepl(",", d) | grepl("\\(", d))]
+		d
+	}
+
+	ff <- list.files(file.path(path, "scripts"), pattern='\\.R$', full.names=TRUE, ignore.case=TRUE)
+	pkgs1 <- unique(unlist(sapply(ff, libfun1)))
+	pkgs2 <- unique(unlist(sapply(ff, libfun2)))
+	
+	pkgs <- unique(c(pkgs1, pkgs2, "readxl", "jsonlite"))
+	
+	ipkgs <- rownames(utils::installed.packages())
+
+	if (!("remotes" %in% ipkgs)) {
+		print(paste("installing remotes"))
+		utils::install.packages("remotes", repos="https://cloud.r-project.org/")
+	}
+	
+	gpkgs <- c("agro", "carobiner")
+	for (pk in gpkgs) {
+		if (!(pk %in% ipkgs)) {
+			print(paste("installing", pk))
+			remotes::install_github(paste0("reagro/", pk))
+		}
+	}
+	ipkgs <- rownames(utils::installed.packages())
+	for (pk in pkgs) {
+	  if (!(pk %in% ipkgs)) {
+		print(paste("installing", pk))
+		utils::install.packages(pkgs=pk, repos="https://cloud.r-project.org/", quiet=TRUE)
+	  }
+	}
+}
+
+
+
 process_carob <- function(path, quiet=FALSE) {
+	get_packages(path)
 	ff <- list.files(file.path(path, "scripts"), pattern="R$", full.names=TRUE)
 	carob_script <- function() {FALSE}
 	for (f in ff) {
