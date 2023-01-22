ad33a296c65d5ffb63dd133c1c2debbea3c6c16f
rhijmans <r.hijmans@gmail.com>
2023-01-21 16:39:59 -0800

    m

--- R/check.R
+++ R/check.R
@@ -140,7 +140,9 @@ check_terms <- function(x, type, path, group="") {
 		for (i in 1:nrow(voc)) {
 			accepted <- utils::read.csv(file.path(path, "terms", paste0(voc$vocabulary[i], ".csv")))[,1]
 			provided <- unique(x[, voc$name[i]])
-			# split by ; for the case there are multiple, if allowed
+			if (voc$required[i] != "yes") {
+				provided <- na.omit(provided)
+			} 
 			if (!is.null(voc$multiple_allowed)) {
 				if (voc$multiple_allowed[i] == "yes") {
 					provided <- unlist(strsplit(provided, "; "))
