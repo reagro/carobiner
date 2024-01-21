1965add3d33be808b33ef07279e90a004d60e0a3
rhijmans <r.hijmans@gmail.com>
2024-01-21 05:14:21 -0800

    reports

---
+++ R/reports.R
@@ -0,0 +1,32 @@
+
+make_reports <- function(path, group="") {
+
+	if (group[1] == "") {
+		group <- get_groups(path)$name
+	} 
+	for (grp in group) {
+		if (grepl("_trials$", grp)) {
+			rmd <- file.path(path, "reports", "trials.Rmd")	
+		} else {
+			rmd <- file.path(path, "reports", paste0(grp, ".Rmd"))
+		}
+		if (!file.exists(rmd)) next
+		rmd <- readLines(rmd, warn=FALSE)
+
+		gpath <- file.path(path, "/data/clean/", grp)
+		ff <- list.files(gpath, pattern="meta.csv$", full=TRUE)
+		uri <- grep("^uri <- ", rmd)
+		on.exit(file.remove(file.path(path, "temp.html")))
+		for (f in ff) {
+			m <- read.csv(f)
+			rmd[uri] <- paste0("uri	 <- '", m$uri, "'")
+			frmd <- file.path(path, "temp.Rmd")
+			writeLines(rmd, frmd)
+			outf <- gsub("_meta.csv", ".html", f)
+			rmarkdown::render(frmd, "html_document", "temp", envir=new.env())
+			file.rename(file.path(path, "temp.html"), outf)
+			print(outf)
+		}
+	}
+}
+
