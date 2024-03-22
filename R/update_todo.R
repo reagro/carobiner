d30fa3c18632eb0d589e5b0099198b9ad9250378
rhijmans <r.hijmans@gmail.com>
2024-03-22 09:47:13 -0700

    todo

--- R/update_todo.R
+++ R/update_todo.R
@@ -8,10 +8,14 @@ update_todo <- function(path) {
 	uri <- gsub("https://doi.org/", "doi:", tolower(todo$uri))
 	uri <- gsub("https://hdl.handle.net/", "hdl:", uri)
 
-	fdone <- list.files(file.path(path, "data", "compiled"), pattern="_metadata.csv$", full.names=TRUE)
-	done <- do.call(bindr, lapply(fdone, utils::read.csv))
+	id <- tolower(sapply(uri, carobiner:::simple_uri, USE.NAMES=F))
+	done <- basename(list.files(file.path(path, "scripts"), pattern="^doi|^hdl.*\\.R$", recursive=TRUE))
+	done <- gsub("\\.r$", "", tolower(done))
+	i <- which(id %in% done)
 
-	i <- unique(stats::na.omit(match(tolower(done$uri), uri)))
+#	fdone <- list.files(file.path(path, "data", "compiled"), pattern="_metadata.csv$", full.names=TRUE)
+#	done <- do.call(bindr, lapply(fdone, utils::read.csv))
+#	i <- unique(stats::na.omit(match(tolower(done$uri), uri)))
 	if (length(i) > 0) {
 #		message(paste("removed", length(i), "datasets from to-do"))
 		todo <- todo[-i,]
