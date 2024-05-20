1ffe95bc3afebb5337e3200c827c8d6462936d9e
rhijmans <r.hijmans@gmail.com>
2024-05-20 07:15:27 -0700

    vars

--- R/check.R
+++ R/check.R
@@ -268,12 +268,15 @@ check_dataset <- function(x, trms, path, answ) {
 	if (grepl("http", x$uri)) {
 		answ[nrow(answ)+1, ] <- c("uri", "http in uri")
 	}
+	if (grepl("http", x$reference)) {
+		answ[nrow(answ)+1, ] <- c("uri", "http in reference")
+	}
 	nms <- trms$name[trms$NAok == "no"]
 	j <- is.na(x[,nms])
 	if (any(j)) {
 		answ[nrow(answ)+1, ] <- c("NA values", paste0("NA in ", paste(nms[j], collapse=", ")))
 	}
-	
+
 	if (isTRUE(nchar(x$publication) > 0 )) {
 		allpubs <- list.files(file.path(path, "references"))
 		pubs <- unlist(strsplit(x$publication, ";|; "))
