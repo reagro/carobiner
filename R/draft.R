75dec4b6787a75aef1d24689165f115a7908ebe0
rhijmans <r.hijmans@gmail.com>
2025-06-27 17:06:38 -0700

    m

--- R/draft.R
+++ R/draft.R
@@ -5,19 +5,21 @@ quotes <- function(x) {
 
 grepaste <- function(pattern, x, ignore.case=TRUE) {
 	v <- grep(pattern, x, ignore.case=ignore.case, value=TRUE)
-	if (length(v) > 1) {
-		v <- paste0(v[1], "\"]], #", paste(v[-1], collapse=", "))
-	} else if (length(v) > 0) {
-		v <- paste0(v, "\"]],")
-	}
+	#if (length(v) > 1) {
+	#	v <- paste0(v[1], "\"]], #", paste(v[-1], collapse=", "))
+	#} else if (length(v) > 0) {
+	#	v <- paste0(v, "\"]],")
+	#}
 	v
 }	
 
 grepr <- function(x) {
 	r <- list(
+		farmer = grepaste("farmer", x),
+		plot = grepaste("plot", x),
 		country = grepaste("country", x),
 		adm1 = grepaste("adm1|region|state|estado", x),
-		adm2 = grepaste("adm2|provinc|distri", x),
+		adm2 = grepaste("adm2|munic|provinc|distri", x),
 		adm3 = grepaste("adm3|ward|commun", x),
 		location = grepaste("locat|village|site", x),
 		site = grepaste("hamlet", x),
@@ -27,8 +29,10 @@ grepr <- function(x) {
 		treatment = grepaste("treat", x),
 		crop = grepaste("crop", x),
 		variety = grepaste("variety|variedad|cultivar|clone", x),
-		planting_date = grepaste("plant.*date", x),
+		year = grepaste("year", x),
+		planting_date = grepaste("plant.*date|sow.*date", x),
 		harvest_date = grepaste("harv.*date", x),
+		maturity_date = grepaste("mat.*date", x),
 		flowering_date = grepaste("flow.*date", x),
 		N_fertilizer = grepaste("^N$", x, FALSE),
 		P_fertilizer = grepaste("^P$", x, FALSE),
@@ -37,6 +41,7 @@ grepr <- function(x) {
 		B_fertilizer = grepaste("^B$", x, FALSE),
 		Mg_fertilizer = grepaste("^Mg$", x, FALSE),
 		Zn_fertilizer = grepaste("^Zn$", x, FALSE),
+		soil_texture =  grepaste("texture", x, FALSE),
 		yield = grepaste("yield", x)
 	)
 	empty <- character(0)
@@ -49,6 +54,160 @@ wdist <- function(x, trms) {
 	a <- apply(d, 1, which.min)
 	z <- data.frame(from=x, to=trms[a], dist=d[cbind(1:nrow(d), a)])
 }
+
+
+get_raw_data <- function(ff) {
+
+	is_xls <- grepl("\\.xlsx$|\\.xls$", ff)
+	out <- list()
+	if (any(is_xls)) {
+		fxls <- ff[is_xls]
+		xls <- vector("list", length(fxls))
+		names(xls) <- basename(fxls)
+		for (i in 1:length(fxls)) {
+			sheets <- readxl::excel_sheets(fxls[i])
+			ds <- lapply(sheets, \(s) carobiner::read.excel(fxls[i], sheet=s))
+			names(ds) <- sheets
+			xls[[i]] <- ds
+		}
+		out$xls <- xls
+	}
+
+	is_csv <- grepl("\\.csv$", ff)
+	if (any(is_csv)) {
+		fcsv <- ff[is_csv]
+		out$csv <- lapply(fscv, \(f) utils::read.csv(f))
+		names(out$csv) <- basename(fcsv)
+	}
+
+	is_dta <- grepl("\\.dta$", ff)
+	if (any(is_dta)) {
+		fdta <- ff[is_dta]
+		out$dta <- lapply(fdta, \(f) haven::read_dta(f))
+		names(out$dta) <- basename(fdta)
+	}
+
+	is_other <- !(is_xls | is_csv | is_dta)
+	if (any(is_other)) {
+		foth <- ff[is_other]
+		out$other <- basename(foth)
+	}
+	out
+}
+
+get_filenames <- function(d) {
+	f <- NULL
+	n <- 0
+	if (!is.null(d$xls)) {
+		fxls <- names(d$xls)
+		n <- length(fxls)
+		f <- paste0(paste0("\tf", 1:n), ' <- ff[basename(ff) == "', fxls, '"]')
+	}
+	if (!is.null(d$csv)) {
+		fcsv <- names(d$csv)
+		n <- n+length(fcsv)
+		f <- c(f, paste0(paste0("\tf", (length(f)+1):n), ' <- ff[basename(ff) == "', fcsv, '"]'))
+	}
+	if (!is.null(d$dta)) {
+		fdta <- names(d$dta)
+		n <- n+length(fdta)
+		f <- c(f, paste0(paste0("\tf", (length(f)+1):n), ' <- ff[basename(ff) == "', basename(fdta), '"]'))
+	}
+	if (!is.null(d$other)) {
+		foth <- names(d$other)
+		n <- n+length(foth)
+		f <- c(f, paste0(paste0("\tf", (length(f)+1):n), ' <- ff[basename(ff) == "', basename(foth), '"]'))
+	}
+	paste(f, collapse="\n")
+}
+
+
+read_files <- function(d) {
+	r <- NULL
+	n <- 1
+	if (!is.null(d$xls)) {
+	# if there is only one xls, and it has multiple sheets, these should be evaluated. 
+		for (i in 1:length(d$xls)) {
+			sheets <- names(d$xls[[i]])
+			if (length(sheets) == 1) {	
+				r <- c(r, paste0(paste0("\tr", n), " <- carobiner::read.excel(f", n, ")"))
+			} else {
+				dst <- utils::adist("data", sheets, ignore.case=TRUE)
+				j <- which.min(dst)
+				sels <- sheets[j]
+				r <- c(r, paste0(paste0("\tr", n, "a"), " <- carobiner::read.excel(f", n, ", sheet=\"", sels, "\")"))
+				sheets <- sheets[-j]
+				for (k in 1:length(sheets)) {
+					r <- c(r, paste0(paste0("\tr", n, letters[k+1]), " <- carobiner::read.excel(f", n, ", sheet=\"", sheets[k], "\")"))
+				}
+			}
+			n <- n + 1
+		}
+	}
+
+	if (any(!is.null(d$csv))) {
+		nn <- length(d$csv)
+		sq <- (n+1):(n+nn)
+		r <- c(r, paste0(paste0("\tr", sq), " <- read.csv(f", sq, ")"))
+		n <- n + nn
+	}
+
+	if (any(!is.null(d$dta))) {
+		nn <- length(is_dta)
+		sq <- (n+1):(n+nn)
+		r <- c(r, paste0(paste0("\tr", sq), " <- read.dta(f", sq, ")"))
+		n <- n + nn
+	}
+	
+	if (any(!is.null(d$other))) {
+		nn <- length(d$other)
+		sq <- (n+1):(n+nn)
+		r <- c(r, paste0(paste0("\tr", sq), " <- read.???(f", sq, ")"))	
+	}
+	
+	paste(r, collapse="\n")
+}
+
+make_unique <- function(x) {
+	n <- unique(x$new)
+	
+	x <- lapply(n, \(new) {
+		y <- x[x$new == new, ]
+		if (nrow(y) > 1) {
+			y <- y[which.min(utils::adist(y$new, y$old[1])), ]
+		}
+		y
+	})
+	do.call(rbind, x)
+}
+
+match_names <- function(r, n) {
+	nms <- names(r)
+	g <- lapply(nms, grepr)
+	x <- sapply(g, length)
+	gg <- unlist(g[x>0])
+	txt <- NULL
+	if (length(gg) > 0) {
+		gg <- data.frame(new=names(gg), old=gg)
+		gg <- make_unique(gg)
+		bod <- unlist(lapply(1:nrow(gg), \(i) paste0("\t\t", gg[i, 1], " = r", n, "[[\"", gg[i,2], "\"]]")))
+		j <- grep("crop", bod)
+		bod[j] <- gsub("]]", "]])", gsub("= ", "= tolower(", bod[j]))
+		j <- grep("$adm", bod)
+		bod[j] <- gsub("]]", "]], \"title\")", gsub("= ", "= fix_name(", bod[j]))
+				
+		bod <- paste(bod, collapse= ",\n")
+		txt <- paste0("\td", n, " <- data.frame(\n", bod, "\n\t)\n")
+		nms <- nms[!(nms %in% gg[,2])]
+	} else {
+		txt <- paste0("\td", n, " <- data.frame()\n")	
+	}
+	if (length(nms) > 0) {
+		txt <- paste0(txt, "##r", n, ": \"", paste0(nms, collapse="\", \""), "\"\n")
+	}
+	txt
+}	
+
 
 
 draft <- function(uri, path, group="draft", overwrite=FALSE) {
@@ -84,108 +243,44 @@ draft <- function(uri, path, group="draft", overwrite=FALSE) {
 	s <- gsub("_pub_", ifelse(is.na(meta$publication), "NA", quotes(meta$publication)), s)
 	s <- gsub("_today_", as.character(as.Date(Sys.time())), s)
 	s <- gsub("_design_", ifelse(is.na(meta$design), "NA", quotes(meta$design)), s)
-	
 
-	f <- NULL
-	n <- 0
-	is_xls <- grepl("\\.xlsx$|\\.xls$", ff)
-	if (any(is_xls)) {
-		fxls <- ff[is_xls]
-		n <- length(fxls)
-		f <- paste0(paste0("\tf", 1:n), ' <- ff[basename(ff) == "', basename(fxls), '"]')
-	}
-	is_csv <- grepl("\\.csv$", ff)
-	if (any(is_csv)) {
-		fcsv <- ff[is_csv]
-		n <- n+length(fcsv)
-		f <- c(f, paste0(paste0("\tf", (length(f)+1):n), ' <- ff[basename(ff) == "', basename(fcsv), '"]'))
-	}
-	is_other <- !(is_xls | is_csv)
-	ef <- f
 
-	if (any(is_other)) {
-		foth <- ff[is_other]
-		n <- n+length(fcsv)
-		f <- c(f, paste0(paste0("\tf", (length(f)+1):n), ' <- ff[basename(ff) == "', basename(foth), '"]'))
-	}
-	wf <- paste(f, collapse="\n")
+	d <- get_raw_data(ff)
+	wf <- get_filenames(d)
+
 	s <- gsub("_filename_", wf, s)
 
-	r <- NULL
-	n <- 1
-	if (any(is_xls)) {
-	# if there is only one xls, and it has multiple sheets, these should be evaluated. 
-		for (i in 1:length(fxls)) {
-			sheets <- readxl::excel_sheets(fxls[i])
-			if (length(sheets) == 1) {	
-				r <- c(r, paste0(paste0("\tr", n), " <- carobiner::read.excel(f", n, ")"))
-			} else {
-				dst <- utils::adist("data", sheets, ignore.case=TRUE)
-				j <- which.min(dst)
-				sels <- sheets[j]
-				r <- c(r, paste0(paste0("\tr", n), " <- carobiner::read.excel(f", n, ", sheet=\"", sels, "\")"))
-				r <- c(r, paste("# other sheets: ", paste(sheets[-j], collapse=", ")))
-			}
-			n <- n + 1
-		}
-	}
-
-	if (any(is_csv)) {
-		nn <- sum(is_csv)
-		sq <- (n+1):(n+nn)
-		r <- c(r, paste0(paste0("\tr", sq), " <- read.csv(f", sq, ")"))
-		n <- n + nn
-	}
-	er <- r
-
-
-	# dta
-	# r <- haven::read_dta(f)
-
-	
-	if (any(is_other)) {
-		nn <- sum(is_other)
-		sq <- (n+1):(n+nn)
-		r <- c(r, paste0(paste0("\tr", sq), " <- read.???(f", sq, ")"))	
-	}
-	r <- paste(r, collapse="\n")
+	r <- read_files(d)
 	s <- gsub("_read_", r, s)
+
+	can_read <- grepl("\\.xlsx$|\\.xls$|\\.csv$|\\.dta", ff)
 	
-	if (is.null(ef)) {
+	if (!any(can_read)) {
 		writeLines(s, fscript)
 		message(fscript)
 		return(invisible(fscript))
 	}
+	d$other <- NULL
 	
-	f <- gsub("\t", "", ef)
-	r <- gsub("\t", "", er)
-	r <- r[!grepl("#", r)]
-	for (i in 1:length(f)) {
-		eval(parse(text = f[i]))
-		eval(parse(text = r[i]))
-	}
-	nms <- lapply(1:length(f), \(i) names(eval(parse(text = paste0("r", i)))))			
-	g <- lapply(nms, grepr)
-	txt <- NULL 
-	for (i in 1:length(g)) {
-		if (length(g[[i]]) > 0) {
-			a <- data.frame(g[[i]])
-			bod <- paste0("\t\t", names(a), " = r", i, "[[\"", a)
-			j <- grep("crop", bod)
-			bod[j] <- gsub("]]", "]])", gsub("= r", "= tolower(r", bod[j]))
-			j <- length(bod)
-			bod[j] <- gsub("]],", "]]", bod[j])
-			bod <- paste(bod, collapse= "\n")
-			txti <- paste0("\td", i, " <- data.frame(\n", bod, "\n\t)\n")
-			txt <- c(txt, txti)
+	nms <- NULL
+	if (!is.null(d$xls)) {
+		for (i in 1:length(d$xls)) {
+			if (length(d$xls[[i]]) == 1) {
+				nms <- c(nms, match_names(d$xls[[i]][[1]], i))
+			} else {
+				for (j in 1:length(d$xls[[i]])) {
+					nms <- c(nms, match_names(d$xls[[i]][[j]], paste0(i, letters[j])))
+				}
+			}
 		}
 	}
 	
-	if (is.null(txt)) {
+	
+	if (is.null(nms)) {
 		s <- gsub("_replacements_", "", s)
 	} else {
-		txt <- paste(txt, collapse="\n")
-		s <- gsub("_replacements_", txt, s)	
+		nms <- paste(nms, collapse="\n\n")
+		s <- gsub("_replacements_", nms, s)	
 	}
 
 	#trms <- vocal::accepted_variables()	
