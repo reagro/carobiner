0663587aac3b438337d711e5743cd93fb13539be
rhijmans <r.hijmans@gmail.com>
2025-07-14 23:08:45 -0700

    m

--- R/check.R
+++ R/check.R
@@ -273,7 +273,7 @@ carob_vocabulary <- function(x=NULL, save=FALSE, add=TRUE, reset=FALSE) {
 
 check_terms <- function(metadata=NULL, records=NULL, longrecs=NULL, wth=NULL, group="", check="all") {
 
-	check_packages("yuri", "0.2-3")
+	check_packages("yuri", "0.2-4")
 	check_packages("vocal", "0.3-4")
 	
 	voc <- carob_vocabulary()
